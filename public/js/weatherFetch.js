c0c19d169eabf0b238a551261c0c8a1e5b08d50e
RileyPiet <rpietenpol@pugetsound.edu>
2022-11-03 15:02:10 -0700

    Added new API that gets 8 days of future weather. Currently just lists this info out in a JSON file

--- public/js/weatherFetch.js
+++ public/js/weatherFetch.js
@@ -1,17 +1,12 @@
 
 let JSONresponse
-let APIkey = "09b5e81e048c36a6df4cad6cdbbef7ed"
-let URL = "https://api.openweathermap.org/data/2.5/weather?lat="
+let APIkey = "234008c0466552aacf5fe3b8a24b6c15"
+//let URL = "https://api.openweathermap.org/data/2.5/forecast?lat="
+let URL = "https://api.openweathermap.org/data/3.0/onecall?lat="
 let inputURL = "http://api.openweathermap.org/geo/1.0/zip?zip="
 let US = 840
 let lat
 let long
-
-//These will be saved after getForecast is called, then they can be utilized elsewhere
-let temperature
-let city
-
-
 
 //Prompts the users location using their location services built in to the computer
 function getLocation(){
@@ -21,11 +16,12 @@ function getLocation(){
         lat = position.coords.latitude;
         long = position.coords.longitude;
     });
-      
+
 }
 
 
 //allows user to input a zipCode and it will return longitude and latitude of that city, these will be sored in lat and long, allowing getForcast(lat,long) to be called and work
+
 async function inputLocation(zipCode){
     let response = await fetch(inputURL + zipCode + "&appid=" + APIkey)
     JSONresponse = await response.json()
@@ -33,29 +29,14 @@ async function inputLocation(zipCode){
     long = JSONresponse.lon
 }
 
+
+
 async function getForecast(lat, long){
-
     //Using lat and long build a string to make an API call
-    
     lat = lat + "&lon="
-    long = long + "&appid="
     //fetch from URL to get weather data
-    let response = await fetch(URL + lat + long + APIkey)
+    console.log(URL + lat + long + "&units=imperial" +  "&appid=" + APIkey)
+    let response = await fetch(URL + lat + long + "&units=imperial" +  "&appid=" + APIkey)
     JSONresponse = await response.json()
-    city = JSONresponse.name
-    //convert Kelvin to farenheit
-    temperature = (JSONresponse.main.temp - 273.15) * 9/5 + 32 
-    console.log("the temperature in " + city + " is " + temperature + " degrees")
-
+    console.log(JSONresponse)
 }
-
-
-
-
-
-
-
-
-
-
-
